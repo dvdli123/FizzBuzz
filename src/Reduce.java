11d420014e63d941ff4f2a8a0d8be8cbd8201e1c
davidli <dvd.li@mail.utoronto.ca>
2023-09-14 17:43:14 -0400

    tutorial task 4, 5

---
+++ src/Reduce.java
@@ -0,0 +1,16 @@
+public class Reduce {
+    public static void main(String[] args) {
+        int i = 100;
+        int ans = 0;
+        while (i != 0){
+            if (i % 2 == 0){
+                i /= 2;
+            }
+            else {
+                i--;
+            }
+            ans++;
+        }
+        System.out.println(ans);
+    }
+}
